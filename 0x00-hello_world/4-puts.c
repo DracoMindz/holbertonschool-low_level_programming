ba9921cadd325b9550e7ff03c17cd45dd67c8983
DracoMindz <704@holbertonschool.com>
2019-02-07 01:16:36 +0000

    Hello World Programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Description: Program uses puts
  *
  * Return: Print Quote
- *
  */
 int main(void)
 {
-    puts ("/"Programming is like building a multilingual puzzle");
+    puts("/"Programming is like building a multilingual puzzle");
     return (0);
 }
