22402a48481582d8724aba24f46bd37f4d431f5c
DracoMindz <704@holbertonschool.com>
2019-02-07 03:20:15 +0000

    Hello World Programming in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("char is %2d bytes \n", sizeof(char));
-printf("int is %2d bytes \n", sizeof(int));
-printf("long int is %2d bytes \n", sizeof(long int));
-printf("long long int is %2d bytes \n", sizeof(long long int));
-printf("float is %2d bytes \n", sizeof(float));
+printf("char is %zd byte(s)\n", sizeof(char));
+printf("int is %zd byte(s)\n", sizeof(int));
+printf("long int is %zd byte(s)\n", sizeof(long int));
+printf("long long int is %zd byte(s)\n", sizeof(long long int));
+printf("float is %zd byte(s)\n", sizeof(float));
 return (0);
 }
