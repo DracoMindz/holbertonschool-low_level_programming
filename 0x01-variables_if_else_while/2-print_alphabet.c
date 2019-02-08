12152e62c06d5e5f173fc15142c7734a970f60b0
DracoMindz <704@holbertonschool.com>
2019-02-08 03:46:52 +0000

    Vaiables in C programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphbet
+ *
+ * Description: Will print lowercase only
+ *
+ * Return: Description of the returned value
+ */
+
+int main()
+{
+	char ch;
+	for(ch = 'a';ch <= 'z';ch++)
+	{
+		printf("%c ",ch);
+	}
+	return 0;
+}
