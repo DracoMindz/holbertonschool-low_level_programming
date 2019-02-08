ecac3634b18d692327a63240c875f44771d0488a
DracoMindz <704@holbertonschool.com>
2019-02-08 21:15:20 +0000

    C PROGRAMMING VARIABLES Redo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print lowercase, then uppercase
+ *
+ * Description: Will print upper and lower cases together
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int ch;
+	for (ch = '0'; ch <= '9', ch++
+		{ putchar(ch);
+		}
+		     putchar('\n');
+	     Return (0);
+}
