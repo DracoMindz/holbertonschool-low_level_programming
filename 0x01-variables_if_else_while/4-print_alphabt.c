82a43b7af57a604d7ed6b0beea3106755a053f96
DracoMindz <704@holbertonschool.com>
2019-02-09 04:33:23 +0000

    Variables Redo

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabets
+ *
+ * Description: print lower case alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0)
+}
