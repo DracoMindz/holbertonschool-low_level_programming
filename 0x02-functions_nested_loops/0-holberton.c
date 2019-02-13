ed514e656c7f0307c26419f602580567926c3739
DracoMindz <704@holbertonschool.com>
2019-02-13 00:35:36 +0000

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - practice with putchar
+ *
+ * Description: print quote use header
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('Holberton');
+	putchar('\n');
+
+	return (0);
+}
