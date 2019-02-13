2ba70434a8730dcf6290af833e9bff33551d8a87
DracoMindz <704@holbertonschool.com>
2019-02-13 06:15:59 +0000

    Functions and Loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
