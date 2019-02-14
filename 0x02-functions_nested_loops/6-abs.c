73d262240decbcf84c1d54c2399c5df897f57be3
DracoMindz <704@holbertonschool.com>
2019-02-14 01:38:13 +0000

    Functions and Loops Redo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _abs - writes the absolute value
+ * @n: The character to print absolute value
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+
+	if (n  > 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 0)
+	{
+		_putchar(n * -1);
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+	return (0);
+}
