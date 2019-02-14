e7a99a359b7455dca774ff6c23118b63cef90bbb
DracoMindz <704@holbertonschool.com>
2019-02-14 02:28:50 +0000

    Functions and Loops Redo

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,17 +8,17 @@
 int _abs(int n)
 {
 
-	if (n  > 0)
+	if (n > 0)
 	{
-		_putchar(n + '0');
+		return (n);
 	}
 		else if (n < 0)
 		{
-			_putchar(n * -1);
+			return (-n);
 		}
 		else
 		{
-			_putchar('0');
+			return (0);
 		}
 
 	return (0);
