087885f785e0a89d2b9a49a9aa99ecf23d4a87da
DracoMindz <704@holbertonschool.com>
2019-02-15 23:03:34 +0000

    More Functions More Loops

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,9 @@
 
 /**
  * print_line - writes the character c to stdout
- * @n: The character to print "_"
+ * @n: The character to print lines
  *
- * Return: n or zero; line or no line
+ * Return: line  or zero; line or no line
  *
  * Description: create a line with the code
  */
@@ -12,12 +12,16 @@ void print_line(int n)
 {
 	int x;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (x = 0; x < n; x++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
