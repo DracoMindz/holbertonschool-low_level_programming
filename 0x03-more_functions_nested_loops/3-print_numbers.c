45003004b68073d4bca5c19c1e2424f90fcc0aa5
DracoMindz <704@holbertonschool.com>
2019-02-15 03:31:51 +0000

    More Functions More Loops

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print  - print numbers
+ *
+ * Description: print numbers
+ *
+ * Return: value
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
