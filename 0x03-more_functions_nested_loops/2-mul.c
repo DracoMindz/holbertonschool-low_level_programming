40eabe5bf96dac61f9b62999aa9a47407e6c8096
DracoMindz <704@holbertonschool.com>
2019-02-15 01:43:00 +0000

    More Functions More Loops

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies integers
+ * @a: multiplier
+ * @b: multiplier
+ *
+ * Description: multiplies integers
+ *
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+	return (0);
+}
