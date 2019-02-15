215dc4f0aa23b777de21f0abe9b4540903af0160
DracoMindz <704@holbertonschool.com>
2019-02-15 04:04:49 +0000

    More Functions More Loops

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Description - numbers 1 to 14  10x
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+			{
+			_putchar(a / 10);
+			_putchar(a % 10);
+			}
+			else
+			{
+				_putchar(a);
+			}
+		}
+	}
+}
