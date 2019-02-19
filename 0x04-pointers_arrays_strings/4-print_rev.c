4f16a851f7b4862d196f55c099fce8b6545516f5
DracoMindz <704@holbertonschool.com>
2019-02-19 21:45:42 +0000

    Pointers, Arrays ans strings

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print _rev - print string in reverse
+ * @*s: pointer to string
+ *
+ * Description: print string in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int str
