f3fc07c46bb8af2496fabe4c43d3cb339d18c242
DracoMindz <704@holbertonschool.com>
2019-02-19 21:57:15 +0000

    Pointers, Arrays ans strings

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - reset value to 98
- * @*n: pointer, valuse at &n
+ * @*n: input
  *
- *Description: function will change the value of  *n
+ * Description: function will change the value of  *n
  *
  * Return: new value 98
  */
