ed60de4c68fc457e2b8ea9dffbe42116c3d7f39a
DracoMindz <704@holbertonschool.com>
2019-02-21 02:00:23 +0000

    Pointers, Arratys, Strings Redo

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half a string
+ * @str: pointer to string
+ *
+ * Description: print half a string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length, half;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		half = (length + 1) / 2;
+	}
+	while (str[half] != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
