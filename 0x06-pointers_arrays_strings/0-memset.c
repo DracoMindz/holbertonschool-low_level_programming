09307b100b36b0d033ef5a68038282f080608c71
DracoMindz <704@holbertonschool.com>
2019-02-25 19:43:59 +0000

    Even More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * memset - fill memeory with a constant byte
+ * @s: pointer
+ * @b: constant number of bytes
+ * @n: number of bytes of memory to be filled
+ *
+ * Description: function fills memeory with a constant byte
+ *
+ * Return: value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for  (x = 0; x < n; x++)
+	{
+		s[x] =  b;
+	}
+	return (s);
+}
