5f69d7147feedcab287579bb1513a13b7e2fcd1d
DracoMindz <704@holbertonschool.com>
2019-02-26 03:15:38 +0000

    Even more pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string pointer
+ * @needle: string to locate in string
+ *
+ * Description: function locates a stringt in a string
+ *
+ * Return: value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *top  = haystack;
+		char *point = needle;
+
+		while (*top && *point && *top  == *point)
+		{
+			return (point);
+			haystack++;
+			point++;
+		}
+		if (point == '\0')
+		{
+			return (haystack);
+		}
+		haystack = top++;
+
+	}
+	return (0);
+}
