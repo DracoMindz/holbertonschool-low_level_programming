603b14d9dea1aca4cdb5d69afbe89aac2772f6c3
DracoMindz <704@holbertonschool.com>
2019-03-01 03:03:32 +0000

    recursion

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+int (root(r + 1, n));
+
+/**
+ * _sqrt_recursion - returns the natural square root
+ * @n: variable
+ *
+ *Description: function returns the natural square root of a number;
+ *
+ * root - multiplies two root to find square
+ * @r - root number and counter
+ *
+ * Description root: function mults two numbers together to find square
+ *
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n >= 0)
+	{
+		return (root(0, n));
+	}
+}
+
+int root(int r, int n)
+{
+		if (r * r == n)
+		{
+			return (r);
+		}
+		if (r * r > n)
+		{
+			return (-1);
+		}
+		return (root(r + 1, n));
+}
