d81c055125c2b4bc36ce8c0d3f5e862b2ec44ef1
DracoMindz <704@holbertonschool.com>
2019-03-03 22:35:20 +0000

    Argc, Argv

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - program prints its name
+ * @argc: contains name of program
+ * @argv: pointer
+ *
+ * Description: program prints name of program
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+	argc++;
+}
