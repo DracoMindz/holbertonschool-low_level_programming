4059396736c3bd7149c410ddbe7dba5b04add2a0
DracoMindz <704@holbertonschool.com>
2019-03-06 04:52:09 +0000

    Malloc and free

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - program creates an array of chars, intializes with char
+ * @size: size of int
+ * @c: variable
+ *
+ * Description: function creates array of chars initializes with specific char
+ *
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int n = 0;
+
+	 char *c = malloc(n * sizeof(size))
+
+	 free(c);
+}
