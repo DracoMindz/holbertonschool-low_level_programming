de03806e7b23f2d7c2e1f5356869f571678c5518
DracoMindz <704@holbertonschool.com>
2019-03-07 01:52:49 +0000

    Malloc and Free

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -11,25 +11,3 @@
  *
  * Return: 0
  */
-
-char *create_array(unsigned int size, char c)
-{
-	unsigned int i = 0;
-	char *str;
-
-	if (size == 0)
-	{
-		return (NULL);
-	}
-	str = malloc(size * sizeof(char));
-	if (str == NULL)
-	{
-		return ('\0');
-	}
-	while (i < size)
-	{
-		str[i] = c;
-		i++;
-	}
-	return (str);
-}
