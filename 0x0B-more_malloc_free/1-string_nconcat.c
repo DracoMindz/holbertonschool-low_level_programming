a334f465cc921adecb920d5da8cef281d58c9651
DracoMindz <704@holbertonschool.com>
2019-03-08 01:33:37 +0000

    More Malloc and free

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -3,11 +3,45 @@
 #include "holberton.h"
 
 /**
- * create_array - program creates an array of chars, intializes with char
- * @size: size of int
- * @c: variable
+ * string_nconcat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Description: function creates array of chars initializes with specific char
+ * Description: function concatenates two strings
  *
- * Return: 0
+ * Return: string
  */
+*string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int a, i, b;
+	char *str;
+
+	if (s1 == 0)
+	{
+		s1 = " ";
+	}
+	else if (s2 == 0)
+	{
+		s2 = " ";
+	}
+	for (a = 0; s1[a] != 0; a++)
+		;
+	for (n = 0; s2[n] != 0; n++)
+	{
+		if (n >= strlen(s2))
+		{
+			n = s2;
+		}
+		n++;
+	}
+	str = malloc(sizeof(char) * (a + n);
+	for (i = 0; i < a; i++)
+	{
+		str[i] = s1[i];
+	}
+		     for (b = 0; b < n; b++, i++)
+		     {
+			     str[i] = s2[b];
+		     }
+	return (str);
+}
