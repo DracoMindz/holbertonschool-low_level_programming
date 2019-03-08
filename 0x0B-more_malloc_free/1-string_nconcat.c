fbf785fa2416bb1013827176fe5983bbb341f15a
DracoMindz <704@holbertonschool.com>
2019-03-08 18:26:37 +0000

    More Malloc and Free Redo

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
+/**
+ * strlen - counts the length of a string
+ *
+ * @s: pointer to a string
+ *
+ * Description: counts the length of a string
+ *
+ * Return: value
+ */
+int strlen(char *s)
+{
+	int x;
+	for (x = 0; s[a] != '\0'; x++)
+		;
+	return (x);
+}
+
 /**
  * string_nconcat - function that concatenates two strings
  * @s1: first string
@@ -15,6 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int a, i, b;
 	char *str;
+	int len;
 
 	if (s1 == 0)
 	{
@@ -24,24 +43,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	for (a = 0; s1[a] != 0; a++)
-		;
-	for (b = 0; s2[b] != 0; b++)
-		;
-	if ((myStrlen(*s2) > n)
-		{
-		}
-	if ((myStrlen(s2) < n)
-		{
-		}
+	if (n < strlen(s1))
+	{
+		len = strlen(s1) + n + 1;
+	}
+	else
+	{
+		len = strlen(s1) + strlen(s2) + 1;
+	}
+	str = malloc(len);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	str = malloc(sizeof(char) * (a + b));
-	for (i = 0; i < a; i++)
+	for (i = 0; s1[i] !=0; i++)
 	{
 		str[i] = s1[i];
 	}
-	for (b = 0; n < b; n++, i++)
+	for (b = 0; s2[b]; b++, i++)
 	{
-		str[i] = s2[n];
+		str[i] = s2[b];
+		ret[i] = '\0';
 	}
 	return (str);
 }
