0de5bd53af37d6410bd8826a9efcb2fdbfc1b348
DracoMindz <704@holbertonschool.com>
2019-03-10 22:51:00 +0000

    Pointer, arrays and strings redo

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,10 @@ char *cap_string(char *n)
 	int i, m;
 	char c[] = " \t\n,;.!?\"(){}";
 
-
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = ((char)n[0] - 'a' + 'A');
+	}
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (m = 0; c[m] != '\0'; m++)
