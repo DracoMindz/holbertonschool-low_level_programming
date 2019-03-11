15b7c2b1aac8ce743a1f3f05cfaea0c37c5d8708
DracoMindz <704@holbertonschool.com>
2019-03-11 19:16:52 +0000

    Preprocessor

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print name of the source file
+ *
+ * Description: print name of source file
+ *
+ * Return: value
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+return (0);
+}
