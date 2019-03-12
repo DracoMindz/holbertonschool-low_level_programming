c74b6eeac8bfe327288b2e3c3df9da507e6a2a23
DracoMindz <704@holbertonschool.com>
2019-03-12 17:57:15 +0000

    Structures

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,20 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-/**
- * struct dog - dog struct
- * @name: dog name
- * @age: dog age
- * @owner: owner
- *
- * Description: dog info struct
- */
-struct dog
-{
-	float age;
-	char *name;
-	char *owner;
-}
+#include "dog.h"
 
 /**
  * init_dog - initiates a struct type variable
@@ -26,9 +12,16 @@ struct dog
  * Description: initiates a struct type
  */
 
-(void) init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-
 /**
  * struct dog - dog struct
  * @age: dog age
@@ -16,4 +15,9 @@ float age;
 char *name;
 char *owner;
 } dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+/*void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d); */
+
 #endif
