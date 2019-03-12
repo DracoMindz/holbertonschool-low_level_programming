dc9cfacdfb9acdedb7e2dc00736e44dbd7b56419
DracoMindz <704@holbertonschool.com>
2019-03-12 00:02:10 +0000

    Structures

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+/**
+ * struct dog - dog struct
+ * @age: dog age
+ * @name: dog name
+ * @owner: dog owner
+ *
+ * Description: dog info struct
+ */
+struct dog
+{
+float age;
+char *name;
+char *owner;
+} dog;
+#endif
