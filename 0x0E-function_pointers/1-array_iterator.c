466d9acd1dccc4c1bfde17fa675c3a222c5446b9
DracoMindz <704@holbertonschool.com>
2019-03-14 00:34:53 +0000

    functions and pointe

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function executes a function
+ * @array: array
+ * @size: size
+ * @action: pointer
+ *
+ * Return: Nothing
+ */
