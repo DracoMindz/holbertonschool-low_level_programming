7aad03721763006ed2b143958269fcd8a9ab4bb7
DracoMindz <704@holbertonschool.com>
2019-03-15 01:11:18 +0000

    Function Pointers

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -1,8 +1,74 @@
-#ifndef _OP_FUNCTIONS_C
-#define _OP_FUNCTIONS_C
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - add two values
+ *
+ * @a: value 1
+ * @b: value 2
+ *
+ * Description: add two values
+ *
+ * Return: value
+ */
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+
+/**
+ * op_sub - subtract two values
+ *
+ * @a: value 1
+ * @b: value 2
+ *
+ * Description: subtract two values
+ * Return: value
+ */
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+
+/**
+ * op_mul - multiply two values
+ *
+ * @a: value 1
+ * @b: value 2
+ *
+ * Description: multiply two values
+ *
+ * Return: value
+ */
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * op_div - divide one value by another
+ *
+ * @a: value 1
+ * @b: value 2
+ *
+ * Desrciption: Divide one value by another
+ *
+ * Return: value
+ */
+int op_div(int a, int b)
+{
+	return ((a) / (b));
+}
+
+/**
+ * op_mod - mod a numer by another
+ *
+ * @a: value 1
+ * @b: value 2
+ * Return: value
+ */
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}
