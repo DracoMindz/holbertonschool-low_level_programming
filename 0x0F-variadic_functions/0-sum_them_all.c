0f59348c230d54c6dbdc88228752f67acfc209a6
DracoMindz <704@holbertonschool.com>
2019-03-17 22:46:45 +0000

    Variadic Functions

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -10,6 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 /*initialize valist for n number od arguements*/
 	va_start(valist, n);
 /*access all arguements assigned to valist*/
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(valist, unsigned int);
