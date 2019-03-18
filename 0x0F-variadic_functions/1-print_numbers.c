38411a38c28331acd73eae4de9e23b2480562278
DracoMindz <704@holbertonschool.com>
2019-03-18 00:34:31 +0000

    Variadic Function

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -5,12 +5,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+	unsigned int num;
+
 /*initialize valist for n number of integers past*/
 	va_start(valist, n);
 /*access all arguments assigned to valis*/
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", i);
+		num = va_arg(valist, unsigned int);
+		printf("%d%s", num, separator);
 	}
 	va_end(valist);
 
