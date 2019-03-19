a727c0e3bf6401bbad640c171ed2f20213e30969
DracoMindz <704@holbertonschool.com>
2019-03-19 17:37:09 +0000

    redo for printf

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,10 +1,26 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct piper - creates a structure
+ * @p: parameter
+ * @Auntie: parameter
+ *
+ */
+typedef struct piper
+{
+	char p;
+	void (*Auntie)();
+
+} peter;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+
 
 #endif
