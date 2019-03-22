0bec1b34927635b870e90f641e9654b485713d19
DracoMindz <704@holbertonschool.com>
2019-03-22 00:09:15 +0000

    Linked List

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <strings.h>
 #include "lists.h"
 /**
  * print_list - function prints lists
@@ -11,7 +12,9 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
-	const list_t *list = h;
+	const list_t *list;
+
+	list = h;
 
 	while (list != NULL)
 	{
@@ -21,10 +24,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-		printf("[%d] %s\n", h->len, list->str);
+			printf("[%d] %s\n", list->len, list->str);
 		}
-		list = list->next;
 		i++;
+		list = list->next;
 	}
 
 	return (i);
