99e3154aa745365db0f8b6690e9217249e98495e
DracoMindz <704@holbertonschool.com>
2019-03-22 05:50:01 +0000

    Linked lists

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -24,27 +24,28 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-/*checking to see if the string is NULL.*/
 	if (new_node->str != '\0')
 	{
-/*check the string for '\0'*/
 		for (i = 0; new_node->str[i] != '\0'; i++)
 		{
 			;
 		}
 	}
+	new_node->len = i;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-
-	new_node->len = i;
-	new_node->next = NULL;
+	else
+	{
 	last_node = *head;
 	while (last_node->next != NULL)
 	{
 		last_node = last_node->next;
+	}
+
 	}
 	last_node->next = new_node;
 
