1cc4f20ad98ebfc9616345767887117414460e59
DracoMindz <704@holbertonschool.com>
2019-03-26 21:50:17 +0000

    More singly linked lists

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,20 +12,16 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = head;
 	unsigned int count = 0;
 
-	if (current != NULL)
+	if (head == NULL)
 	{
-		if (count == index)
-		{
-			return (current->n);
-			count++;
-			current = current->next;
-		}
+		return (NULL);
 	}
-	else
+	while (count < index)
 	{
-	return (NULL);
+		head = head->next;
+		count++;
 	}
+	return (head);
 }
