171ea077355234a15ef4445a20cdef0b7d9300ba
DracoMindz <704@holbertonschool.com>
2019-03-26 02:12:33 +0000

    More singly linked lists

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,12 +16,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last_node;
 
 	new_node = malloc(sizeof(listint_t));
-	last_node = *head;
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -35,8 +37,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			last_node = last_node->next;
 		}
 
+		last_node->next = new_node;
 	}
-	last_node->n = n;
-
 	return (last_node);
 }
