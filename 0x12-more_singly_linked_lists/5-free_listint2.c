bcd4f98871a669a8cb9d440ced51cbbc6eabcdce
DracoMindz <704@holbertonschool.com>
2019-03-26 02:45:41 +0000

    More singly linked lists

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function frees a list
+ * @head: list of ints
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
