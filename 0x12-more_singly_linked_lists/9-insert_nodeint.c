c5696ba428b4774a27a8f67b6024a6a7c0c15d81
DracoMindz <704@holbertonschool.com>
2019-03-27 19:30:43 +0000

    More singly linked lists redo

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function inserts a new node at a given position.
+ * @head: pointer to list of ints at index
+ * @idx: index of node
+ * @n: data
+ *
+ * Return: address of new node or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *pointerNode = *head;
+	listint_t *node;
+
+	unsigned int count = 1;
+
+	if (head == NULL) /*If head points to NULL(if no linked list exists*/
+		return (NULL);
+	if (idx == 0)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		if (*head == NULL)
+		{
+			*head = node;
+			node->next = NULL;
+		}
+		else if (*head != NULL)
+		{
+		node->next = *head;
+		*head = node;
+		}
+		return (node);
+	}
+
+	for (; count < idx - 1 && pointerNode != NULL; count++)
+	{
+		pointerNode = pointerNode->next;
+	}
+	if (pointerNode == NULL)
+		return (NULL);
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = pointerNode->next;
+	pointerNode->next = node;
+
+	return (node);
+}
