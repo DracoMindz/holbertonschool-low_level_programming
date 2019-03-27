398ed9a7d2b3d6e9796c48bd1e4727239282c7de
DracoMindz <704@holbertonschool.com>
2019-03-27 22:06:28 +0000

    Redo

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *pointerNode;
 	listint_t *node;
 
-	unsigned int count = 1;
+	unsigned int count = 0;
 
 	if (head == NULL) /*If head points to NULL(if no linked list exists*/
 		return (NULL);
