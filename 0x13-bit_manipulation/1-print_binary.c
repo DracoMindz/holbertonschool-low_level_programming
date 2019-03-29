fbc84f9c235422f69b080d145b7735c8218e6dfc
DracoMindz <704@holbertonschool.com>
2019-03-29 03:40:36 +0000

    bit manipulation

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_binary - check the code for Holberton School students.
+ * @n: number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n);
+{
+	unsigned long int mask = 1;
+	return (0);
+}
