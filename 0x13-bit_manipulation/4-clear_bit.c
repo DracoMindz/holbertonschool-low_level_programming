05a42682c7e3720da9d788a311009ecaec4302cf
DracoMindz <704@holbertonschool.com>
2019-03-30 02:37:02 +0000

    Bit Manipulation

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
-	if (n == NULL)
+	if (index > 63)
 		return (-1);
 
-	if (index > 63)
+	if (n == NULL)
 		return (-1);
 
 	mask = mask << index;
