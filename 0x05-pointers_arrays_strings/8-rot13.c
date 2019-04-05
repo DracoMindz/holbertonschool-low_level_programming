640a1de714a8b2c6b8fe4a71ab4ea65b3fde39c7
DracoMindz <704@holbertonschool.com>
2019-04-05 01:44:24 +0000

    Redo

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -2,7 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * rot13 - function shift numbers 13 to left
+ * @r: pointer to astring
  *
  * Return: Always 0.
  */
+char *rot13(char *)
+{
+	int x, y;
+
+	char arr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmUpperCase";
+
+	for (x = 0; r[x]; x++)
+	{
+		for (y = 0; arr[y]; y++)
+		{
+			if (r[x] == arr[y])
+			{
+				arr[x] == arr2[y];
+				break;
+			}
+		}
+	}
+}
