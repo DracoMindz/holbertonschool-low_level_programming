00cb4b88dd28e76e2013e831e8e7c03dd0af6d15
DracoMindz <704@holbertonschool.com>
2019-04-06 02:47:26 +0000

    Redo

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -21,34 +21,35 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int i;
 	int n;
-	int w;
+	int w = 0;
 
 
 
 	if (filename == NULL)
 		return (-1);
-	if (t == NULL || *t == "")
-	fd = open(filename, O_CREAT | O_RDONLY, 0600);
-	if (fd == -1)
+	if (text_content == NULL || *text_content == '\0')
 	{
+		fd = open(filename, O_CREAT | O_RDONLY, 0600);
+		if (fd == -1)
+		{
 		return (-1);
+		}
+		close(fd);
+		return (1);
 	}
-	close(fd);
-	return (1);
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; t[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	do {
-		n = write(fd, t + w, i - w)
-			if (n < 1)
-				return (0);
-		n += w;
+		n = write(fd, text_content + w, i - w);
+		if (n < 1)
+		{
+		return (-1);
+		}
+		w += n;
 	} while (w < i);
 	close(fd);
 	return (1);
-
-
 }
