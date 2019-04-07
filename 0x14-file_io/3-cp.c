41ecf78e135a8c509b427c6ad1107c687cd19144
DracoMindz <704@holbertonschool.com>
2019-04-07 02:34:35 +0000

    redo

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "holberton.h"
 #define BSIZE 1024
-
 /**
  * main - program copies content of a file to another file
  * @argc: contains arguement passed through program
@@ -8,47 +13,57 @@
  *
  * Return: data
  */
-
 int main(int argc, char **argv)
 {
 	int fd0, fd1, fd2;
-	char *file_to, char *file_from, char *buffer;
+	char *file_to, *file_from, *buffer;
 	ssize_t bytes_read, bytes_written, close_error;
 
 	if (argc != 3)
 	{
-		dprintf(STUDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = argv[1];
 	file_to = argv[2];
-	fd0 = open(file_to, O_WRONGLY | O_CREAT | O_TRUNC, 0664);
-	fd1 = open(file_from, O_RDONLY);
-	if (fd1 == -1)
+	if ((fd1 = open(file_from, O_RDONLY)) == -1))
 	{
 		dprintf(STUDERR_FILENO, "Error: Can't write to %\n", file_from);
 		exit(98);
 	}
-	do {
+	if ((fd0 = open(file_to, O_WRONGLY | O_CREAT | O_TRUNC, 0664)) == -1)
+	{
+		dprintf(STUDERR_FILENO, "Error: Can't write to %\n", file_to);
+		return (99);
+	}
+	do{
 		bytes_read = read(file_from, buffer, BSIZE);
-	if (bytes_read == 0)
-		break;
-	if (bytes_read == 1)
-		if (file_from == NULL || *file_from == '\0')
-			return (98);
-		bytes_written = write(to, buffer, bytes_read);
-	if (bytes_written < 1)
-		if (file_to == NULL || *file_to == '\0')
+		if (bytes_read == -1)
+		{
+			dprintf(STUDERR_FILENO, "Error: Can't write to %\n", file_to);
 			return (99);
 		}
-	while (1)
+		if (bytes_read == 0)
+		{
+			break;
+		}
+		bytes_written = write(to, buffer, bytes_read);
+		if (bytes_written < 1)
+		{
+			dprintf(STUDERR_FILENO, "Error: Can't write to %\n", file_to);
+			return (99);
+		}
+	} while (1);
+	if ((close_error = close(file_to)) == -1)
 	{
-	close_error = close(file_to);
-	if (close_error == -1)
 		dprintf(STDERR_FILENO, "Can't read from file_to %d", file_to);
-	close_error = close(file_from);
-	if (close_error == -1)
+		return (100);
+	}
+	if (close_error = close(file_from); == -1)
+	{
 		dprintf(STDERR_FILENO, "Cant' read from file_from %d", file_from);
+		return (100);
 	}
 	return (0);
 }
+
