22187eaf3cd66ef14f25c7beadc0d90ac74d2570
DracoMindz <704@holbertonschool.com>
2019-05-06 19:20:03 +0000

    Dynamic Libraries

--- 0x17-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x17-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -fPIC -pedantic -Werror -Wextra -c *.c
-gcc -Wall -shared -pedantic -Werror -Wextra -o liball.so *.o
+gcc -Wall -fPIC -c *.c
+gcc -Wall -shared -Wl -pedantic -Werror -Wextra -o liball.so *.o
