9c9570191477c2f9e60577338ad5f26f63248efb
DracoMindz <704@holbertonschool.com>
2019-05-09 07:25:05 +0000

    Nth Node Doubly Linked Lists

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function returns nth node of a dlinked list
+ * @head: list
+ * @index: index of node strting at 0
+ *
+ * Return: nth node of list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (count < index)
+	{
+		head = head->next;
+		count++;
+	}
+	return (head);
+}
