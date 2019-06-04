c73854a43264764cb187858b276310ffc83d681e
DracoMindz <704@holbertonschool.com>
2019-06-04 02:13:14 +0000

    Hash Tables

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,11 +1,37 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - function that retrieves a value associated with a key.
+ *@ht: hash table
+ *@key: key looking for
+ * Return: value associated with key
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	const char *copy_key;
+	unsigned long int index;
+	hash_node_t node;
 
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	copy_key = strdup(key);
+	index = key_index(key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, copy_key) == 0)
+		{
+			break;
+		}
+	node = node->next;
+	}
+	free(copy_key);
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	return (node->value);
 }
