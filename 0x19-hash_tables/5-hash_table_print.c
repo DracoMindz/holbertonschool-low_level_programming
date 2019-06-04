670dfb690dcb9ef7c9cbbfbf15ffea3bce66dda3
DracoMindz <704@holbertonschool.com>
2019-06-04 03:05:40 +0000

    Hash Tables

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,11 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table to be printed
  *
- * Return: 
+ * Return: hash table in the order array, list
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i;
 
+	if (ht == NULL)
+	{
+		;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			printf("%s, %s\n", array[i]->key, array[i]->value);
+		}
+	}
 }
