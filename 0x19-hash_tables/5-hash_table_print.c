5b1b5702d461fc457cf4959977744086d2d75e49
DracoMindz <704@holbertonschool.com>
2019-06-09 21:59:08 +0000

    Hash Tables

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -8,17 +8,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i, j;
+	hash_node_t *node;
 
 	if (ht == NULL)
 	{
-		;
+		return;
 	}
+	node = ht->array[0];
+	putchar('{');
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		for (; node != NULL; node = node->next)
 		{
-			printf("%s, %s\n", array[i]->key, array[i]->value);
+			if (j == 1)
+			printf(",");
+			printf("'%s': '%s'", node->key, node->value);
+			j = 1;
 		}
 	}
+	puts("}");
 }
