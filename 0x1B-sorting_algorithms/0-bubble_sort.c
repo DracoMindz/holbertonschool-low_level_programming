2d1834c72e62051c733bdb3c1e885dfbdd921f8c
DracoMindz <veganengineering@gmail.com>
2019-06-18 19:20:20 +0000

    Sorting Algorithms & Big O

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+/**
+ * bubble_sort - Functino sorts an array of integers in ascending order using Bubble Sort
+ *
+ * @array: array to be sorted
+ * @size: number of elements in an array
+ *
+ * Return: void
+ */
 
 void bubble_sort(int *array, size_t size)
 {
  
-  temp;
-  a = array[0]
+         int a, b, x;
+	 
+	 for(a = 0; a < size-1; a++);
+	 {
+	   for(b = 0; b < size - b - 1; b++)
+	     {
+	       if(array[b] > array[b - 1])
+		 {
+		   x = array[b];
+		   array[b] = array[b - 1];
+		   array[b-1] = x;
+		 }
+	     }
+	 }
+}
+
     
   
 
