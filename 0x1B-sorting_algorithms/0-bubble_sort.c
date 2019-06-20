6cf15e9de9dc7ad268b70669f32c020e5ed28322
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-06-20 14:53:43 -0400

    C sort Bubble

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
 /**
  * bubble_sort - Functino sorts an array of integers in ascending order using Bubble Sort
  *
@@ -13,27 +29,28 @@
 void bubble_sort(int *array, size_t size)
 {
  
-     int a, b, x;
-	 
-	for(a = 0; a < size-1; a++);
+     int flag, x;
+	 size_t a, b;
+	if (array == NULL || size < 2)
+		return; 
+	for(a = 0; a < size-1; a++)
 	{
+		flag = 0;
 	   	for(b = 0; b < size - 1; b++)
 	    {
-	    	if (array[b] < array[b + 1])
-            {
-                x = array[b];
-                print_array(array, size);
-                array[b] = array[b + 1];
-                array[b + 1] = x;
-            }
 			if (array[b] > array[b + 1])
             {
+				/* printf("got here!!")*/
 				x = array[b + 1];
-		      	print_array(array, size);
 		   		array[b + 1] = array[b];
-		   		array[b + 1] = x;
+		   		array[b] = x;
+				print_array(array, size);
+				flag = -1;
 			}
 		}
+		if (flag == 0)
+			return;
+		b = 0;
     }
 }
     
