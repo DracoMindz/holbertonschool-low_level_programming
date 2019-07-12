05eeecc1042967540bc518587f32b0fee7c71a4f
DracoMindz <veganengineering@gmail.com>
2019-07-12 01:00:34 +0000

    Island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+#Island Perimeter Problem
+
 
 def island_perimeter(grid):
     numZeros = 0
 
-    for  currentRowDex in range(len(grid)):
+    for currentRowDex in range(len(grid)):
         for currentColDex in range(len(grid[0])):
             if grid[currentRowDex][currentColDex] == 1:
 
@@ -11,7 +13,7 @@ def island_perimeter(grid):
                 numLeftColDex = currentColDex - 1
                 numBottomRowDex = currentRowDex + 1
                 numRightColDex = currentColDex + 1
-            
+
                 if numTopRowDex < 0:
                     numTop = 0
                 else:
@@ -30,14 +32,14 @@ def island_perimeter(grid):
                 if numRightColDex >= len(grid[0]):
                     numRight = 0
                 else:
-                    numRight = grid[ currentRowDex][currentColDex + 1]
+                    numRight = grid[currentRowDex][currentColDex + 1]
         
                 if numTop == 0:
                     numZeros += 1
                 if numRight == 0:
                     numZeros += 1
                 if numBottom == 0:
-                    numZeros +=1
+                    numZeros += 1
                 if numLeft == 0:
                     numZeros += 1
 
