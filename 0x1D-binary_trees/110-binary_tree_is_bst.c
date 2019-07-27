1cb04990237d25144c46c569e24fc069668f187c
DracoMindz <DracoMindz@github.com>
2019-07-27 01:42:42 +0000

    Binary trees

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -4,7 +4,7 @@
  * bst_T *bst_insert - function that inserts a value in a Binary Search Tree
  *
  * @tree: double pointer to root node of the BST to insert the value
- * @value: value to store node to be inserted
+ * @value: value to store node to insert
  *
  * Return: Value
  */
@@ -15,11 +15,14 @@ int binary_tree_is_bst(const binary_tree_t *tree)
       binary_tree_is_bst(tree->left);
       binary_tree_is_bst(tree->right);
       
-      if (tree->left != NULL && tree->right != NULL)
+      if (tree->left != NULL && tree->left->n < tree->n)
+	return (1);
+
+      if (tree->right != NULL && tree->right->n > tree->n)
 	return (1);
   
-      if (tree->left < tree->right)
-	return (1);
+      if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
+	return (0);
 
       else 
 	return (0);
