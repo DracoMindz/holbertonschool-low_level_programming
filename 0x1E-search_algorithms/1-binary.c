64689adf7e04e1edfae40c05e7bd6da461738638
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-30 13:59:11 -0400

    Update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,9 +2,9 @@
 
 /**
 * binary_search - searches for a value in array, use Binary search
-*@array: pointer to first element of array
-*@size: number of elements
-*@value: value to find
+* @array: pointer to first element of array
+* @size: number of elements
+* @value: value to find
 * Return: first index of value or NULL
 */
 
@@ -12,38 +12,40 @@ int binary_search(int *array, size_t size, int value)
 {
 	if (!array)
 		return (-1);
-	return (bin_srch(array, 0, size - 1, value));
+	return (binary_search(array, size - 1, value));
 }
 /**
 * binary_searcher - function searches for value in array
-*@array: array of values to be searched
-*@first: first element in the array
-*@last: last element in the array
+* @array: array of values to be searched
+* @first: first element in the array
+* @last: last element in the array
+* @value: value to find
+* Return: int value
 */
 
 int binary_searcher(int *array, size_t first, size_t last, int value)
 {
-        size_t a_index;
-        int mid;
+	size_t a_index;
+	int mid;
 
-        printf("Searching in array: ");
-        for (a_index = first; a_index <= last; a_index++)
-        {
-                printf("%d", array[a_index]);
-                if (a_index < last)
-                        printf(", ");
-                if (a_index == last)
-                        printf("\n");
-        }
-        mid = ((last + first) / 2);
-        if (array[mid] == value)
-                return (mid);
-        if (last > first)
-        {
-                if (value < array[mid])
-                        return (binary_searcher(array, first, mid - 1, value));
-                else if (value > array[mid])
-                        return (binary_searcher(array, mid + 1, last, value));
-        }
-        return (-1);
+	printf("Searching in array: ");
+	for (a_index = first; a_index <= last; a_index++)
+	{
+		printf("%d", array[a_index]);
+		if (a_index < last)
+			printf(", ");
+		if (a_index == last)
+			printf("\n");
+	}
+	mid = ((last + first) / 2);
+	if (array[mid] == value)
+		return (mid);
+	if (last > first)
+	{
+		if (value < array[mid])
+			return (binary_searcher(array, first, mid - 1, value));
+		else if (value > array[mid])
+			return (binary_searcher(array, mid + 1, last, value));
+	}
+	return (-1);
 }
